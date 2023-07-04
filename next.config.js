d35179d42dd451caa2bc0e2197b33fda033b59bd
awsome-dev-journey <koalah86@gmail.com>
2023-07-04 14:45:50 +0100

    update: add domain to fetch image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['cdn2.thecatapi.com'],
+    deviceSizes: [480, 768, 1025, 1260, 1440],
+    imageSizes: [480, 768, 1025, 1260, 1440],
+    // loader: 'custom',
+  },
   reactStrictMode: true,
 };
 
