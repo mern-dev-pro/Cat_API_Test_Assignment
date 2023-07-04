6934c6739b6191fc7c72a113cffbf157b497de6d
awsome-dev-journey <koalah86@gmail.com>
2023-07-04 14:48:44 +0100

    create: favour page

---
+++ src/pages/favour.tsx
@@ -0,0 +1,10 @@
+import FavourPage from '@/components/_pages/Favour';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main>
+      <FavourPage />
+    </main>
+  );
+}
